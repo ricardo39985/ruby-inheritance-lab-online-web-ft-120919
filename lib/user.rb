5bdceb3f497d971f398cdc47f739cce682d588f2
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 23:21:28 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,10 @@
 class User
   attr_accessor :first_name, :last_name
   @@all = []
-  def initialize(first, last)
-    @first_name = first
-    @last_name = last
-    @all << self
-
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @@all << self
   end
 
 end
